2e1a0e96a444ccb66937220a2c961872a5ee5ff0
Francisco Jordano <arcturus@ardeenelinfierno.com>
2012-09-23 20:48:37 +0100

    Initial import

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    ptc = require('point-to-city');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    var lat = req.query['lat'] || null,
+	lon = req.query['lon'] || null;
+    if (!lat || !lon) {
+        res.send('Missing required parameters lat,lon');
+	return;
+    }
+    var query = ptc.init('[yourappidhere]');
+    var method = query.point_to_city;
+    if (req.query['full']) {
+    	method = query.yahoo_where;
+    }
+    method.call(this, lat, lon, function(result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.write(JSON.stringify(result));
+    },
+    function(error) {
+    	res.send('Error : ' + JSON.stringify(error));
+    });
+});
+app.listen(3000);
